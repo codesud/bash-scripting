f454c36a84c8e8fa0e24a8352d61191b28c18682
CodingSudeep <sudeepdirect@gmail.com>
2022-08-10 13:50:23 +0530

    This is my first commit.

---
+++ 01-sample.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# First line shouls always be Shebang notation. There should be only one Shebang notion, apart from that # is referred as comment.
+echo "Hello World"
+
+## Printing Multiple lines
+
+echo "Hai"
+echo "Hello"
+
+## \n : New Line ;  ## \t : Tab Space 
+
+echo -e "Hai\n\tHello"
