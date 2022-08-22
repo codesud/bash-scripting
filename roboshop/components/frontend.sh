c52129efcafdf04b180ce8d95d3db92eb3168d66
CodingSudeep <sudeepdirect@gmail.com>
2022-08-22 23:13:02 +0530

    Auto push commit.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+USER_ID = $(id -u)
+if [$USER_ID -ne 0]; then 
+    echo -e "\e[13m You need to run this as root user \e[0m"
+    exit 1
+fi
+
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
