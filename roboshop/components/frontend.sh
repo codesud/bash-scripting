05f38971c4c68c5a2f8c665673df29d5b2db72b2
CodingSudeep <sudeepdirect@gmail.com>
2022-08-23 18:24:30 +0530

    Auto push commit.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,30 +6,30 @@ LOGFILE="/temp/$COMPONENT.log"
 
 source components/common.sh
 
-echo -n "Installing Nginx :"
+echo -n "Installing Nginx : "
 yum install nginx -y &>> LOGFILE
 stat $? 
 
 systemctl enable nginx &>> LOGFILE
 
-echo -n "Starting Nginx :"
+echo -n "Starting Nginx : "
 systemctl start nginx &>> LOGFILE
 stat $? 
 
-echo -n "Downloading $COMPONENT :"
+echo -n "Downloading $COMPONENT: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $? 
 
-echo -n "Clearing the old contents :"
+echo -n "Clearing the old contents: "
 cd /usr/share/nginx/html
 rm -rf *
 stat $? 
 
-echo -n "Extracting $COMPONENT :"
+echo -n "Extracting $COMPONENT: "
 unzip /tmp/frontend.zip &>> LOGFILE
 stat $? 
 
-echo -n "Updating the PROXY file :"
+echo -n "Updating the PROXY file: "
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -4,41 +4,43 @@ set -e
 COMPONENT=mongodb
 LOGFILE="/temp/$COMPONENT.log"
 MONGODB_REPO_URL="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+COMPONENT_REPO="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 
 source components/common.sh
 
-echo -n "Downloading $COMPONENT repo :"
+echo -n "Downloading $COMPONENT repo: "
 curl -s -o /etc/yum.repos.d/mongodb.repo $MONGODB_REPO_URL
 stat $? 
 
 
-echo -n "Installing $COMPONENT :"
+echo -n "Installing $COMPONENT: "
 yum install -y mongodb-org &>> LOGFILE
 stat $? 
 
-echo -n "Updating $COMPONENT Listening address :"
+echo -n "Updating $COMPONENT Listening address: "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
-echo -n "Starting $COMPONENT :"
+echo -n "Starting $COMPONENT: "
 systemctl enable mongod &>> LOGFILE
 systemctl restart mongod &>> LOGFILE
 stat $?
 
+echo -n "Downloading $COMPONENT code: "
+curl -s -L -o /tmp/mongodb.zip "$COMPONENT_REPO" 
+stat $?
+
+cd /tmp
+
+echo -n "Extracting $COMPONENT code: "
+unzip mongodb.zip &>> LOGFILE
+stat $?
+
+cd mongodb-main
+
+echo -n "Injecting $COMPONENT schema: "
+mongo < catalogue.js &>> LOGFILE
+mongo < users.js &>> LOGFILE
+stat $?
+
 echo -e " ___________ \e[32m $COMPONENT Configuration is completed \e[0m ___________ "
-
-
-# Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
-# Config file:   # vim /etc/mongod.conf
-
-
-# systemctl restart mongod &>> LOGFILE
-
-
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-
-# cd /tmp
-# unzip mongodb.zip &>> LOGFILE
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
