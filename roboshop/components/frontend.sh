9ff7f4419da2bf331e0065b8731aad09c4ace38e
CodingSudeep <sudeepdirect@gmail.com>
2022-08-23 16:32:20 +0530

    Auto push commit.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,29 +6,39 @@ LOGFILE="/temp/$COMPONENT.log"
 
 source components/common.sh
 
+stat() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m SUCCESS \e[0m"
+    else 
+        echo -e "\e[31m FAILURE \e[0m"
+    fi
+}
+
 echo -n "Installing Nginx :"
 yum install nginx -y &>> LOGFILE
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m SUCCESS \e[0m"
-else 
-    echo -e "\e[31m FAILURE \e[0m"
-fi
+stat $? 
 
-systemctl enable nginx
+systemctl enable nginx &>> LOGFILE
 
 echo -n "Starting Nginx :"
-systemctl start nginx
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m SUCCESS \e[0m"
-else 
-    echo -e "\e[31m FAILURE \e[0m"
-fi
+systemctl start nginx &>> LOGFILE
+stat $? 
 
+echo -n "Downloading $COMPONENT"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+echo -n "Clearing the old contents of $COMPONENT"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
+stat $? 
+
+echo -n "Extracting $COMPONENT"
+unzip /tmp/frontend.zip &>> LOGFILE
+stat $? 
+
+echo -n "Updating the PROXY file"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $? 
