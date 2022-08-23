d3432b180c014185fbd4b076fdfa30f4580f7d33
CodingSudeep <sudeepdirect@gmail.com>
2022-08-23 16:35:19 +0530

    Auto push commit.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -24,20 +24,20 @@ echo -n "Starting Nginx :"
 systemctl start nginx &>> LOGFILE
 stat $? 
 
-echo -n "Downloading $COMPONENT"
+echo -n "Downloading $COMPONENT :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $? 
 
-echo -n "Clearing the old contents of $COMPONENT"
+echo -n "Clearing the old contents :"
 cd /usr/share/nginx/html
 rm -rf *
 stat $? 
 
-echo -n "Extracting $COMPONENT"
+echo -n "Extracting $COMPONENT :"
 unzip /tmp/frontend.zip &>> LOGFILE
 stat $? 
 
-echo -n "Updating the PROXY file"
+echo -n "Updating the PROXY file :"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
