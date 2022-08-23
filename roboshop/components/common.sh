d0e85342d38f46c0494d8531c360bc09cb43e79f
CodingSudeep <sudeepdirect@gmail.com>
2022-08-23 16:58:02 +0530

    Auto push commit.

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -5,3 +5,11 @@ if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[13m You need to run this as root user \e[0m"
     exit 1  
 fi
+
+stat() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m SUCCESS \e[0m"
+    else 
+        echo -e "\e[31m FAILURE \e[0m"
+    fi
+}
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,14 +6,6 @@ LOGFILE="/temp/$COMPONENT.log"
 
 source components/common.sh
 
-stat() {
-    if [ $1 -eq 0 ] ; then
-        echo -e "\e[32m SUCCESS \e[0m"
-    else 
-        echo -e "\e[31m FAILURE \e[0m"
-    fi
-}
-
 echo -n "Installing Nginx :"
 yum install nginx -y &>> LOGFILE
 stat $? 
