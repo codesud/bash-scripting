b026ae77eccee19fc0db62c2145ee85bcf77e3f6
CodingSudeep <sudeepdirect@gmail.com>
2022-08-26 20:16:37 +0530

    Auto push commit.

--- roboshopdirect/components/common.sh
+++ roboshopdirect/components/common.sh
@@ -67,7 +67,7 @@ DOWNLOAD_AND_EXTRACT() {
 CONFIG_SERVICE() {
     echo -n "Configuring $COMPONENT service: "
     sed -i -e "s/REDIS_ENDPOINT/redis.$APPUSER.internal/" -e "s/MONGO_ENDPOINT/mongodb.$APPUSER.internal/" -e "s/MONGO_DNSNAME/mongodb.$APPUSER.internal/" systemd.service
-    mv /home/$APPUSER/$COMPONENT/systemd.service  /etc/systemd/system/$COMPONENT.service
+    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
     stat $?
 
 }
