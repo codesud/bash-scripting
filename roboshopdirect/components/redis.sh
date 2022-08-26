db078d462aa0cebe74dd7543f2b89dd36ae49348
CodingSudeep <sudeepdirect@gmail.com>
2022-08-26 17:39:32 +0530

    Auto push commit.

--- roboshopdirect/components/redis.sh
+++ roboshopdirect/components/redis.sh
@@ -8,11 +8,11 @@ source components/common.sh
 
 echo -n "Downloading $COMPONENT repo: "
 # curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-stat() $?
+stat $?
 
 echo -n "Installing $COMPONENT : "
 # yum install redis-6.2.7 -y
-stat() $?
+stat $?
 
 echo -n "Updating $COMPONENT Listening address: "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
