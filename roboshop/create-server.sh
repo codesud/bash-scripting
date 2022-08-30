b1886d36ca8d15fb9681461fbf346b0a2af1c829
CodingSudeep <sudeepdirect@gmail.com>
2022-08-31 01:47:51 +0530

    Auto push commit.

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -6,7 +6,7 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-Ce
 echo $AMI_ID 
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-id $SGID --tag-specifications "ResourceType=instance,Tags=
-[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].PrivateIpAddress'
+[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 )
 
 echo $PRIVATE_IP
