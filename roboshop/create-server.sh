3fe49aeb8e6f4340ed8e11e8d5f40f752a7bdcb1
CodingSudeep <sudeepdirect@gmail.com>
2022-08-30 23:00:51 +0530

    Auto push commit.

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -4,4 +4,4 @@ COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]"  | jq
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]"  | jq
