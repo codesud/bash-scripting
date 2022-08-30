50b2c66d1d8d57dfbf2fab8e5cb5a3db5bfa0da5
CodingSudeep <sudeepdirect@gmail.com>
2022-08-30 22:03:14 +0530

    Auto push commit.

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID 
