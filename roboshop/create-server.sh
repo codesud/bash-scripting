e335d2a6b1a97edd142fc32af33ef1cf4dc2edbf
CodingSudeep <sudeepdirect@gmail.com>
2022-08-31 01:41:59 +0530

    Auto push commit.

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -4,4 +4,13 @@ COMPONENT=$1
 SGID="sg-09566ba4e8fe56dc5"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-id $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq
+
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-id $SGID --tag-specifications "ResourceType=instance,Tags=
+[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].PrivateIpAddress'
+)
+
+echo PRIVATE_IP
+
+# #Changing the IPADRESS and DNS Name as per component name
+# sed -e 's/IPADDRESS/${PRIVATE_IP}/' -e 's/COMPONENT/${COMPONENT}/' route53.json > /tmp/record.json
+# aws route53 change-resource-record-sets --hosted-zone-id Z02995003SL9D4FIIDWN4 --change-batch file:///tmp/record.json | jq
