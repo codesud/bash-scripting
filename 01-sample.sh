f81654aacdecc0092fbda38818578c0879b73800
Sudeep S <sudeepdirect@gmail.com>
2022-11-26 15:47:53 +0530

    Updated bash files

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# First line shouls always be Shebang notation. There should be only one Shebang notion, apart from that # is referred as comment.
+# First line should always be Shebang notation. There should be only one Shebang notion, apart from that # is referred as comment.
 echo "Hello World"
 
 ## Printing Multiple lines
