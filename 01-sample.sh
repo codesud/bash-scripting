b518c6695c3ec0a3e3d0239a555790c9cdc2ec8b
Sudeep S <sudeepdirect@gmail.com>
2022-11-26 17:35:38 +0530

    Updated bash files

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # First line should always be Shebang notation. There should be only one Shebang notion, apart from that # is referred as comment.
-echo "Hello World"
+echo "Hello World!!!"
 
 ## Printing Multiple lines
 
-echo "Hai"
-echo "Hello"
+echo "Hai!!!"
+echo "Hello!!!"
 
 ## \n : New Line ;  ## \t : Tab Space 
 
-echo "Hai\n\tHello"
+echo -e "Hai!!!\n\tHello!!!"
+echo "Dwell on the beauty of life. Watch the stars, and see yourself running with them. "
